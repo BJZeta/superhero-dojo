9316871dd89850891e6f01c3f749de34b29f75fa
Brandon Unzueta <unzueta.brandon@gmail.com>
2021-02-23 16:28:20 -0800

    redesigning look for app

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Form, Container, Button } from "react-bootstrap";
+import { Form, Container, Button, Row, Col, Image } from "react-bootstrap";
 import { listCharacters } from "../actions/searchActions";
 import SearchCard from "../components/SearchCard";
 
@@ -18,29 +18,46 @@ const SearchPage = () => {
   };
 
   return (
-    <Container md={6}>
-      <Form onSubmit={handleSearchCharacter}>
-        <Form.Group controlId="formCharacterSearch">
-          <Form.Label>Search Character</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter a Hero/Villain"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <Form.Text className="text-muted">
-            If a character is appearing, try using their Alias, i.e. Dick
-            Grayson, Miles Morales
-          </Form.Text>
-          <Button variant="primary" type="submit">
-            Search
+    <Container>
+      <Row>
+        <Col md={12}>
+          <Form onSubmit={handleSearchCharacter}>
+            <Form.Group controlId="formCharacterSearch">
+              <Form.Label>Search Character</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter a Hero/Villain"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+              <Form.Text className="text-muted" as="p">
+                If a character is appearing, try using their Alias, i.e. Dick
+                Grayson, Miles Morales. Also, try hyphens (Spider-Man)
+              </Form.Text>
+              <Button variant="primary" type="submit">
+                Search
+              </Button>
+            </Form.Group>
+          </Form>
+          {loading && <h1>Loading....</h1>}
+          {error && <h3>{error}</h3>}
+          {characters &&
+            characters.map((character) => <SearchCard character={character} />)}
+        </Col>
+      </Row>
+      <Row>
+        <Col md={4} xs={6} className="float-left">
+          <Image className="float-left" src="https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081" width="80%" />
+        </Col>
+        <Col  md={4}>
+          <Button>
+            Button
           </Button>
-        </Form.Group>
-      </Form>
-      {loading && <h1>Loading....</h1>}
-      {error && <h3>{error}</h3>}
-      {characters &&
-        characters.map((character) => <SearchCard character={character} />)}
+        </Col>
+        <Col md={4} xs={6} >
+          <Image className="float-right" src="https://cdn.shopify.com/s/files/1/0533/2089/files/placeholder-images-image_large.png?format=jpg&quality=90&v=1530129081" width="80%" />
+        </Col>
+      </Row>
     </Container>
   );
 };
